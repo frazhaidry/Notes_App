14205570c34268354f4f183905ee57badd5e22f6
frazhaidry <frazhaidry24@gmail.com>
2025-07-06 17:25:08 +0530

    .

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "./constants";
 
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: process.env.REACT_APP_API_URL,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
